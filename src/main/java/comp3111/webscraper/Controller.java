abe932b6f32fe66032a6b51ea145de01e8e3199b
Hanif Dean <mhdnadhif@connect.ust.hk>
2018-10-10 22:58:24 +0800

    Added labelCount text in order to see the difference when making a pull request from Hanif's branch

--- src/main/java/comp3111/webscraper/Controller.java
+++ src/main/java/comp3111/webscraper/Controller.java
@@ -69,6 +69,7 @@ public class Controller {
     		output += item.getTitle() + "\t" + item.getPrice() + "\t" + item.getUrl() + "\n";
     	}
     	textAreaConsole.setText(output);
+    	labelCount.setText("Hi");
     	
     	
     }
